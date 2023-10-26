0ce47508b3c02f92b303e6a9267f9346c36f5ce3
Cade Lawless <clawless05@gmail.com>
2023-10-26 16:49:32 -0500

    added filter feature

--- buyer-view.php
+++ buyer-view.php
@@ -8,6 +8,24 @@ require("includes/classes.php");
 require("includes/error-functions.php");
 // database connection
 $db = new DB();
+
+// initialize filter variables
+$valid_options = ["1", "2"];
+$sort_priority = $_SESSION["sort_priority"] ?? "1";
+$sort_price = $_SESSION["sort_price"] ?? "";
+
+// if filter value is changed, change session value
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $sort_priority = $_POST["sort_priority"];
+    $sort_price = $_POST["sort_price"];
+    if(!in_array($sort_priority, $valid_options)){
+        $_SESSION["sort_priority"] = $sort_priority;
+    }
+    if(!in_array($sort_price, $valid_options)){
+        $_SESSION["sort_price"] = $sort_price;
+    }
+    //header("Location: buyer-view.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -23,14 +41,42 @@ $db = new DB();
         <?php require "includes/background.php"; ?>
         <h1 class="center">Cade's Christmas Wishlist</h1>
         <div id="container">
+            <h2 class='center'>All Items</h2>
+            <form class="filter-form" method="POST" action="">
+                <div class="filter-input">
+                    <label for="sort-priority">Sort by Priority</label><br>
+                    <select id="sort-priority" name="sort_priority">
+                        <option value="">None</option>
+                        <option value="1" <?php if($sort_priority == "1") echo "selected"; ?>>Highest to Lowest</option>
+                        <option value="2" <?php if($sort_priority == "2") echo "selected"; ?>>Lowest to Highest</option>
+                    </select>
+                </div>
+                <div class="filter-input">
+                    <label for="sort-price">Sort by Price</label><br>
+                    <select id="sort-price" name="sort_price">
+                        <option value="">None</option>
+                        <option value="1" <?php if($sort_price == "1") echo "selected"; ?>>Lowest to Highest</option>
+                        <option value="2" <?php if($sort_price == "2") echo "selected"; ?>>Highest to Lowest</option>
+                    </select>
+                </div>
+            </form>
             <?php
-            echo "<h2 class='center'>All Items</h2>";
-            if(isset($_GET["pageno"])){
-                $pageno = $_GET["pageno"];
+            if(isset($_SESSION["pageno"])){
+                $pageno = $_SESSION["pageno"];
             }else{
                 $pageno = 1;
             }
-            paginate("buyer", $db, "SELECT * FROM items ORDER BY purchased ASC", 12, $pageno);
+            $priority_order = match ($sort_priority) {
+                "" => "",
+                "1" => ", priority ASC",
+                "2" => ", priority DESC",
+            };
+            $price_order = match ($sort_price) {
+                "" => "",
+                "1" => ", price * 1 ASC",
+                "2" => ", price * 1 DESC",
+            };
+            paginate("buyer", $db, "SELECT * FROM items ORDER BY purchased ASC$priority_order$price_order, date_added DESC", 12, $pageno);
             ?>
         </div>
     </div>
@@ -56,5 +102,12 @@ $db = new DB();
         x.addEventListener("click", function(){
             x.parentElement.parentElement.parentElement.parentElement.classList.add("hidden");
         })
+    }
+
+    // submit form on filter change
+    for(const sel of document.querySelectorAll("select")){
+        sel.addEventListener("change", function(){
+            document.querySelector("form").submit();
+        });
     }
 </script>
