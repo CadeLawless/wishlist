e51328625b5f71c0fce2fd99d53dfcecf5673000
Cade Lawless <clawless05@gmail.com>
2024-10-16 09:44:35 -0500

    added password requirements and fixed pattern

--- create-an-account.php
+++ create-an-account.php
@@ -29,6 +29,7 @@ if(isset($_POST["submit_button"])){
         }
     }
     $password = errorCheck("password", "Password", "Yes", $errors, $error_list);
+    patternCheck(regex: "/^(?=.*[0-9])(?=.*[a-zA-Z])(.+){6,}$/", input: $password, errors: $errors, error_list: $error_list, msg: "<li>Password does not meet the requirements.</li>");
     $confirm_password = errorCheck("confirm_password", "Confirm Password", "Yes", $errors, $error_list);
     if($password != "" && $confirm_password != "" && $password != $confirm_password){
         $errors = true;
@@ -65,31 +66,80 @@ if(isset($_POST["submit_button"])){
 </head>
 <?php require("includes/body-open-tag.php"); ?>
     <div id="body">
-        <?php require "includes/background.php"; ?>
-        <p class="center"><img class="logo login" src="images/site-images/logo.png" alt="Wish List" /></p>
-        <form id="login-form" method="POST" action="">
-            <p><a href="login.php">Back to Login</a></p>
-            <?php if(isset($error_msg)) echo $error_msg?>
-            <div class="large-input center">
-                <label for="name">First Name: </label><br>
-                <input required type="text" name="name" value="<?php echo $name?>" id="name">
-            </div>
-            <div class="large-input center">
-                <label for="username">Username: </label><br>
-                <input required type="text" name="username" value="<?php echo $username?>" id="username">
-            </div>
-            <div class="large-input center">
-                <label for="password">Password: </label><br>
-                <input required type="password" name="password" id="password" value="<?php echo $password?>" pattern=".*[a-zA-Z]+\d+.*">
-            </div>
-            <div class="large-input center">
-                <label for="confirm_password">Confirm Password: </label><br>
-                <input required type="password" name="confirm_password" value="<?php echo $confirm_password?>" id="confirm_password">
-                <span class="error-msg hidden">Passwords must match</span>
-            </div>
-            <p class="large-input center"><input type="submit" class="button text" name="submit_button" value="Start Wishing"></p>
-        </form>
+        <div id="container">
+            <p class="center"><img class="logo login" src="images/site-images/logo.png" alt="Wish List" /></p>
+            <form id="login-form" method="POST" action="">
+                <p><a href="login.php">Back to Login</a></p>
+                <?php if(isset($error_msg)) echo $error_msg?>
+                <div class="large-input center">
+                    <label for="name">First Name: </label><br>
+                    <input required type="text" name="name" value="<?php echo $name?>" id="name">
+                </div>
+                <div class="large-input center">
+                    <label for="username">Username: </label><br>
+                    <input required type="text" name="username" value="<?php echo $username?>" id="username">
+                </div>
+                <div class="large-input">
+                    <div style="margin: 0 0 22px;">
+                        Password Requirements:
+                        <ul>
+                            <li>Must include at least one letter and one number</li>
+                            <li>Must be at least 6 characters long</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="large-input center">
+                    <label for="password">Password: </label><br>
+                    <div class="password-input">
+                        <input required type="password" name="password" id="password" value="<?php echo $password?>" pattern="^(?=.*[0-9])(?=.*[a-zA-Z])(.+){6,}$">
+                        <span class="password-view hide-password hidden"><?php require("images/site-images/icons/hide-view.php"); ?></span>
+                        <span class="password-view view-password"><?php require("images/site-images/icons/view.php"); ?></span>
+                        <span class="error-msg hidden">Please match the requirements</span>
+                    </div>
+                </div>
+                <div class="large-input center">
+                    <label for="confirm_password">Confirm Password: </label><br>
+                    <input required type="password" name="confirm_password" value="<?php echo $confirm_password?>" id="confirm_password">
+                    <span class="error-msg hidden">Passwords must match</span>
+                </div>
+                <p class="large-input center"><input type="submit" class="button text" name="submit_button" value="Start Wishing"></p>
+            </form>
+        </div>
     </div>
 </body>
 <?php include "includes/footer.php"; ?>
 </html>
+<script>
+$(document).ready(function(){
+    $("input").on("input", function(){
+        if(this.validity.patternMismatch){
+            setTimeout(() => {
+                if(this.validity.patternMismatch){
+                    if($(this).hasClass("price-input")){
+                        $(this).parent().addClass("invalid");
+                    }else{
+                        $(this).addClass("invalid");
+                    }
+                }
+            }, 2500);
+        }else{
+            if($(this).hasClass("price-input")){
+                $(this).parent().removeClass("invalid");
+            }else{
+                $(this).removeClass("invalid");
+            }
+        }
+    });
+
+    $(".view-password").on("click", function(){
+        $("#password, #confirm_password").attr("type", "text");
+        $(this).addClass("hidden");
+        $(".hide-password").removeClass("hidden");
+    });
+    $(".hide-password").on("click", function(){
+        $("#password, #confirm_password").attr("type", "password");
+        $(this).addClass("hidden");
+        $(".view-password").removeClass("hidden");
+    });
+});
+</script>
