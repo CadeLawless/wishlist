4bf95a8f7ce3e69d00be883b6af0f00d74603eb1
Cade Lawless <clawless05@gmail.com>
2024-10-16 10:13:58 -0500

    added remember me to login

--- login.php
+++ login.php
@@ -11,6 +11,7 @@ $db = new DB();
 // initialize form field variables
 $username = "";
 $password = "";
+$remember_me = "No";
 
 // submit php
 if(isset($_POST["submit_button"])){
@@ -19,16 +20,26 @@ if(isset($_POST["submit_button"])){
     $errorList = "";
     $username = errorCheck("username", "Username", "Yes", $errors, $errorList);
     $password = errorCheck("password", "Password", "Yes", $errors, $errorList);
+    $remember_me = isset($_POST["remember_me"]) ? "Yes" : "No";
     if(!$errors){
         $findUser = $db->select("SELECT password FROM wishlist_users WHERE username = ?", [$username]);
         if($findUser->num_rows > 0){
             while($row = $findUser->fetch_assoc()){
                 $hashed_password = $row["password"];
                 if(password_verify($password, $hashed_password)){
-                    $expire_date = date("Y-m-d H:i:s", strtotime("+1 year"));
-                    if($db->write("UPDATE wishlist_users SET session = ?, session_expiration = ? WHERE username = ?", [session_id(), $expire_date, $username])){
-                        $cookie_time = (3600 * 24 * 365); // 1 year
-                        setcookie("wishlist_session_id", session_id(), time() + $cookie_time);
+                    if($remember_me == "Yes"){
+                        $expire_date = date("Y-m-d H:i:s", strtotime("+1 year"));
+                        $sql = "UPDATE wishlist_users SET session = ?, session_expiration = ? WHERE username = ?";
+                        $values = [session_id(), $expire_date, $username];
+                    }else{
+                        $sql = "UPDATE wishlist_users SET session = NULL, session_expiration = NULL WHERE username = ?";
+                        $values = [$username];
+                    }
+                    if($db->write($sql, $values)){
+                        if($remember_me == "Yes"){
+                            $cookie_time = (3600 * 24 * 365); // 1 year
+                            setcookie("wishlist_session_id", session_id(), time() + $cookie_time);
+                        }
                         $_SESSION["wishlist_logged_in"] = true;
                         $_SESSION["username"] = $username;
                         header("Location: index.php");
@@ -74,6 +85,10 @@ if(isset($_POST["submit_button"])){
                 <label for="password">Password: </label><br>
                 <input required type="password" name="password" id="password">
                 <span class="error-msg hidden">Password must include</span>
+            </div>
+            <div class="large-input">
+                <input type="checkbox" id="remember_me" name="remember_me" <?php if($remember_me == "Yes") echo "checked"; ?> />
+                <label style="float: none; font-weight: normal;" for="remember_me">Remember me</label>
             </div>
             <p class="large-input center"><input type="submit" class="button text" name="submit_button" value="Login"></p>
             <p style="font-size: 14px" class="large-input center">Dont have an account? <a href="create-an-account.php">Create one here</a></p>
