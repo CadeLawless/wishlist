ebc346437de79e3299e46052afa97b21c1d07139
Cade Lawless <clawless05@gmail.com>
2025-10-10 09:57:42 -0500

    fix: Configure proper MVC structure with public directory for static assets
    - Update .htaccess to serve static files from public directory
    - Add rewrite rule for public/(css|js|images|scripts)/ paths
    - Update script paths to use public/js/ directory
    - Follow proper MVC convention where static assets are in public/
    - This maintains separation between application code and public assets

--- views/wishlist/show.php
+++ views/wishlist/show.php
@@ -522,14 +522,14 @@ $price_order = $sort_price ? "price {$sort_price}, " : "";
     </div>
 </body>
 </html>
-<script src="js/popups.js"></script>
-<script src="js/copy-link.js"></script>
-<script src="js/copy-select.js"></script>
-<script src="js/checkbox-selection.js"></script>
-<script src="js/wishlist-filters.js"></script>
-<script src="js/wishlist-pagination.js"></script>
+<script src="public/js/popups.js"></script>
+<script src="public/js/copy-link.js"></script>
+<script src="public/js/copy-select.js"></script>
+<script src="public/js/checkbox-selection.js"></script>
+<script src="public/js/wishlist-filters.js"></script>
+<script src="public/js/wishlist-pagination.js"></script>
 <script>$type = "wisher"; $key_url = "";</script>
-<script src="js/page-change.js"></script>
-<script src="js/choose-theme.js"></script>
-<script src="js/filter-change.js"></script>
+<script src="includes/page-change.js"></script>
+<script src="includes/choose-theme.js"></script>
+<script src="includes/filter-change.js"></script>
 
