b963a808c89dc96fee01fa17027d16124c31f46b
Cade Lawless <clawless05@gmail.com>
2025-10-10 09:53:46 -0500

    fix: Move JavaScript files to includes directory for proper web server access
    - Copy all custom JS files from public/js/ to includes/
    - Update script paths to use includes/ directory
    - This matches the existing structure where popup.js and other JS files are located
    - Should resolve 404 errors for JavaScript files

--- views/wishlist/show.php
+++ views/wishlist/show.php
@@ -522,14 +522,14 @@ $price_order = $sort_price ? "price {$sort_price}, " : "";
     </div>
 </body>
 </html>
-<script src="/wishlist/public/js/popups.js"></script>
-<script src="/wishlist/public/js/copy-link.js"></script>
-<script src="/wishlist/public/js/copy-select.js"></script>
-<script src="/wishlist/public/js/checkbox-selection.js"></script>
-<script src="/wishlist/public/js/wishlist-filters.js"></script>
-<script src="/wishlist/public/js/wishlist-pagination.js"></script>
+<script src="includes/popups.js"></script>
+<script src="includes/copy-link.js"></script>
+<script src="includes/copy-select.js"></script>
+<script src="includes/checkbox-selection.js"></script>
+<script src="includes/wishlist-filters.js"></script>
+<script src="includes/wishlist-pagination.js"></script>
 <script>$type = "wisher"; $key_url = "";</script>
-<script src="/wishlist/includes/page-change.js"></script>
-<script src="/wishlist/includes/choose-theme.js"></script>
-<script src="/wishlist/includes/filter-change.js"></script>
+<script src="includes/page-change.js"></script>
+<script src="includes/choose-theme.js"></script>
+<script src="includes/filter-change.js"></script>
 
