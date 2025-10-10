90de18fc1ba173cc1606ee3e044959cfd12dbe5a
Cade Lawless <clawless05@gmail.com>
2025-10-10 08:07:25 -0500

    fix: Add all 4 buttons to initial page load
    - Update static template in views/wishlist/show.php to match dynamic generation
    - Add View Details button with fullscreen popup
    - Add Website Link button
    - Keep Edit and Delete buttons
    - Now initial load shows all 4 buttons like AJAX pagination
    - Fixes issue where buttons only appeared after page changes

--- views/wishlist/show.php
+++ views/wishlist/show.php
@@ -489,6 +489,43 @@ $price_order = $sort_price ? "price {$sort_price}, " : "";
                                                 <div class='line'><h4 class='notes-label'>Notes: </h4><span><?php echo htmlspecialchars(substr($item['notes'], 0, 30)) . (strlen($item['notes']) > 30 ? '...' : ''); ?></span></div>
                                                 <div class='line'><h4 class='notes-label'>Priority: </h4><span>(<?php echo $item['priority']; ?>)</span></div>
                                                 <div class='icon-options item-options wisher-item-options'>
+                                                    <a class='icon-container popup-button' href='#'>
+                                                        <?php require(__DIR__ . '/../../images/site-images/icons/view.php'); ?>
+                                                        <div class='inline-label'>View</div>
+                                                    </a>
+                                                    <div class='popup-container hidden'>
+                                                        <div class='popup fullscreen'>
+                                                            <div class='close-container'>
+                                                                <a href='#' class='close-button'>
+                                                                <?php require(__DIR__ . '/../../images/site-images/menu-close.php'); ?>
+                                                                </a>
+                                                            </div>
+                                                            <div class='popup-content'>
+                                                                <h2 style='margin-top: 0;'>Item Details</h2>
+                                                                <p><label>Item Name:<br /></label><?php echo htmlspecialchars($item['name']); ?></p>
+                                                                <p><label>Item Price:<br /></label>$<?php echo htmlspecialchars($item['price']); ?></p>
+                                                                <p><label>Website Link:<br /></label><a target='_blank' href='<?php echo htmlspecialchars($item['link']); ?>'>View on Website</a></p>
+                                                                <p><label>Notes: </label><br /><?php echo nl2br(htmlspecialchars($item['notes'])); ?></p>
+                                                                <p><label>Priority:<br /></label>(<?php echo $item['priority']; ?>) <?php 
+                                                                    $priorities = [
+                                                                        1 => "absolutely needs this item",
+                                                                        2 => "really wants this item", 
+                                                                        3 => "It would be cool if they had this item",
+                                                                        4 => "Eh, they could do without this item"
+                                                                    ];
+                                                                    echo $priorities[$item['priority']] ?? '';
+                                                                ?></p>
+                                                                <p><label>Date Added:<br /></label><?php echo date("n/j/Y g:i A", strtotime($item['date_added'])); ?></p>
+                                                                <?php if($item['date_modified']): ?>
+                                                                <p><label>Last Date Modified:</label><br /><?php echo date("n/j/Y g:i A", strtotime($item['date_modified'])); ?></p>
+                                                                <?php endif; ?>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <a class='icon-container' href='<?php echo htmlspecialchars($item['link']); ?>' target='_blank'>
+                                                        <?php require(__DIR__ . '/../../images/site-images/icons/link.php'); ?>
+                                                        <div class='inline-label'>Website Link</div>
+                                                    </a>
                                                     <a class='icon-container' href='/wishlist/edit-item.php?id=<?php echo $item['id']; ?>&pageno=<?php echo $pageno; ?>'>
                                                         <?php require(__DIR__ . '/../../images/site-images/icons/edit.php'); ?>
                                                         <div class='inline-label'>Edit</div>
