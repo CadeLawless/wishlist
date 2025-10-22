6bc02547279d47d67578ea927985c6709f2b945a
Cade Lawless <clawless05@gmail.com>
2025-10-22 12:39:43 -0500

    Fix jQuery not defined error on login page
    - Add jQuery library to auth layout (views/layouts/auth.php)
    - Fix CSS and favicon paths to use public/ directory
    - Login page now has access to jQuery for password visibility toggle
    - Resolves '$ not defined' error on authentication pages

--- views/layouts/auth.php
+++ views/layouts/auth.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="images/site-images/favicon.ico">
-    <link rel="stylesheet" type="text/css" href="css/styles.css" />
-    <link rel="stylesheet" type="text/css" href="css/snow.css" />
+    <link rel="icon" type="image/x-icon" href="public/images/site-images/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="public/css/styles.css" />
+    <link rel="stylesheet" type="text/css" href="public/css/snow.css" />
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <title><?php echo $title ?? 'Wish List'; ?></title>
 </head>
 <body class="<?php echo isset($user['dark']) && $user['dark'] === 'Yes' ? 'dark' : ''; ?>">
