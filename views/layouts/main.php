87eb5b3ee46cf2abff4663275b911eeddcc701d7
Cade Lawless <clawless05@gmail.com>
2025-10-22 08:17:04 -0500

    Fix CSS and favicon paths in main layout
    - Update CSS links to point to public/css/ instead of css/
    - Update favicon link to point to public/images/site-images/
    - Ensure static assets are loaded from correct public directory

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -3,17 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="images/site-images/favicon.ico">
-    <link rel="stylesheet" type="text/css" href="css/styles.css" />
-    <link rel="stylesheet" type="text/css" href="css/snow.css" />
+    <link rel="icon" type="image/x-icon" href="public/images/site-images/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="public/css/styles.css" />
+    <link rel="stylesheet" type="text/css" href="public/css/snow.css" />
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <title><?php echo $title ?? 'Wish List'; ?></title>
     <style>
         #body {
-            padding-top: 84px;
+            padding-top: 130px;
         }
         h1 {
-            display: inline-block;
             margin-top: 0;
         }
         h2 {
