21fa6c896d56992a59489e9ba6449b71ba38b90b
Cade Lawless <clawless05@gmail.com>
2025-10-27 12:57:24 -0500

    Fix home page for unauthenticated users
    - Show login/register buttons for guests instead of wishlist buttons
    - Prevents confusing redirect-to-login behavior
    - Maintains existing functionality for authenticated users
    - Improves user experience for new visitors

--- views/home/index.php
+++ views/home/index.php
@@ -15,9 +15,19 @@ if($account_created){
     </div>";
 }
 ?>
-<div class="big-buttons-container">
-    <a class="big-button create-wish-list" href="/wishlist/create"><?php require(__DIR__ . "/../../public/images/site-images/icons/plus.php"); ?>Create Wish List</a>
-    <a class="big-button view-wish-list" href="/wishlist/wishlists"><?php require(__DIR__ . "/../../public/images/site-images/icons/search.php"); ?>View Wish Lists</a>
-</div>
+
+<?php if($user): ?>
+    <!-- Authenticated user content -->
+    <div class="big-buttons-container">
+        <a class="big-button create-wish-list" href="/wishlist/create"><?php require(__DIR__ . "/../../public/images/site-images/icons/plus.php"); ?>Create Wish List</a>
+        <a class="big-button view-wish-list" href="/wishlist/wishlists"><?php require(__DIR__ . "/../../public/images/site-images/icons/search.php"); ?>View Wish Lists</a>
+    </div>
+<?php else: ?>
+    <!-- Guest user content -->
+    <div class="big-buttons-container">
+        <a class="big-button login-button" href="/wishlist/login"><?php require(__DIR__ . "/../../public/images/site-images/icons/view.php"); ?>Login</a>
+        <a class="big-button register-button" href="/wishlist/register"><?php require(__DIR__ . "/../../public/images/site-images/icons/plus.php"); ?>Create Account</a>
+    </div>
+<?php endif; ?>
 
 <script src="js/popup.js"></script>
