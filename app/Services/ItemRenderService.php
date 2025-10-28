2ef2b1df6e012a1c5339efc66c6b7272ba1046a8
Cade Lawless <clawless05@gmail.com>
2025-10-28 07:21:51 -0500

    feat: hide purchase details for purchased items in buyer view
    - Hide price, notes, priority, view button, and website link for purchased items
    - Only show item name and gift wrap for purchased items in buyer view
    - Maintains clean appearance for already-purchased items
    - Prevents confusion about purchasing already-bought items

--- app/Services/ItemRenderService.php
+++ app/Services/ItemRenderService.php
@@ -60,42 +60,48 @@ class ItemRenderService
             </div>
             <div class='item-description'>
                 <div class='line'><h3><?php echo $itemNameShort; ?></h3></div>
-                <div class='line'><h4>Price: $<?php echo $price; ?> <span class='price-date'>(as of <?php echo $dateModified ? date("n/j/Y", strtotime($dateModified)) : date("n/j/Y", strtotime($dateAdded)); ?>)</span></h4></div>
+                <?php if(!($type === 'buyer' && $item['purchased'] === 'Yes')): ?>
+                    <div class='line'><h4>Price: $<?php echo $price; ?> <span class='price-date'>(as of <?php echo $dateModified ? date("n/j/Y", strtotime($dateModified)) : date("n/j/Y", strtotime($dateAdded)); ?>)</span></h4></div>
+                <?php endif; ?>
                 <?php if($type === 'wisher'): ?>
                     <div class='line'><h4 class='notes-label'>Quantity Needed:</h4> <?php echo $quantity; ?></div>
                 <?php endif; ?>
-                <div class='line'><h4 class='notes-label'>Notes: </h4><span><?php echo $notesShort; ?></span></div>
-                <div class='line'><h4 class='notes-label'>Priority: </h4><span>(<?php echo $item['priority']; ?>) <?php echo $priorityText; ?></span></div>
+                <?php if(!($type === 'buyer' && $item['purchased'] === 'Yes')): ?>
+                    <div class='line'><h4 class='notes-label'>Notes: </h4><span><?php echo $notesShort; ?></span></div>
+                    <div class='line'><h4 class='notes-label'>Priority: </h4><span>(<?php echo $item['priority']; ?>) <?php echo $priorityText; ?></span></div>
+                <?php endif; ?>
                 <div class='icon-options item-options <?php echo $type; ?>-item-options'>
-                    <a class='icon-container popup-button' href='#'>
-                        <?php require(__DIR__ . '/../../public/images/site-images/icons/view.php'); ?>
-                        <div class='inline-label'>View</div>
-                    </a>
-                    <div class='popup-container hidden'>
-                        <div class='popup fullscreen'>
-                            <div class='close-container'>
-                                <a href='#' class='close-button'>
-                                    <?php require(__DIR__ . '/../../public/images/site-images/menu-close.php'); ?>
-                                </a>
-                            </div>
-                            <div class='popup-content'>
-                                <h2 style='margin-top: 0;'>Item Details</h2>
-                                <p><label>Item Name:<br /></label><?php echo $itemName; ?></p>
-                                <p><label>Item Price:<br /></label>$<?php echo $price; ?></p>
-                                <p><label>Website Link:<br /></label><a target='_blank' href='<?php echo $link; ?>'>View on Website</a></p>
-                                <p><label>Notes: </label><br /><?php echo nl2br($notes); ?></p>
-                                <p><label>Priority:<br /></label>(<?php echo $item['priority']; ?>) <?php echo $priorityText; ?></p>
-                                <p><label>Date Added:<br /></label><?php echo $dateAdded; ?></p>
-                                <?php if($dateModified): ?>
-                                <p><label>Last Date Modified:</label><br /><?php echo $dateModified; ?></p>
-                                <?php endif; ?>
+                    <?php if(!($type === 'buyer' && $item['purchased'] === 'Yes')): ?>
+                        <a class='icon-container popup-button' href='#'>
+                            <?php require(__DIR__ . '/../../public/images/site-images/icons/view.php'); ?>
+                            <div class='inline-label'>View</div>
+                        </a>
+                        <div class='popup-container hidden'>
+                            <div class='popup fullscreen'>
+                                <div class='close-container'>
+                                    <a href='#' class='close-button'>
+                                        <?php require(__DIR__ . '/../../public/images/site-images/menu-close.php'); ?>
+                                    </a>
+                                </div>
+                                <div class='popup-content'>
+                                    <h2 style='margin-top: 0;'>Item Details</h2>
+                                    <p><label>Item Name:<br /></label><?php echo $itemName; ?></p>
+                                    <p><label>Item Price:<br /></label>$<?php echo $price; ?></p>
+                                    <p><label>Website Link:<br /></label><a target='_blank' href='<?php echo $link; ?>'>View on Website</a></p>
+                                    <p><label>Notes: </label><br /><?php echo nl2br($notes); ?></p>
+                                    <p><label>Priority:<br /></label>(<?php echo $item['priority']; ?>) <?php echo $priorityText; ?></p>
+                                    <p><label>Date Added:<br /></label><?php echo $dateAdded; ?></p>
+                                    <?php if($dateModified): ?>
+                                    <p><label>Last Date Modified:</label><br /><?php echo $dateModified; ?></p>
+                                    <?php endif; ?>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <a class='icon-container' href='<?php echo $link; ?>' target='_blank'>
-                        <?php require(__DIR__ . '/../../public/images/site-images/icons/link.php'); ?>
-                        <div class='inline-label'>Website Link</div>
-                    </a>
+                        <a class='icon-container' href='<?php echo $link; ?>' target='_blank'>
+                            <?php require(__DIR__ . '/../../public/images/site-images/icons/link.php'); ?>
+                            <div class='inline-label'>Website Link</div>
+                        </a>
+                    <?php endif; ?>
                     <?php if($type === 'wisher'): ?>
                         <a class='icon-container' href='/wishlist/<?php echo $wishlistId; ?>/item/<?php echo $item['id']; ?>/edit?pageno=<?php echo $page; ?>'>
                             <?php require(__DIR__ . '/../../public/images/site-images/icons/edit.php'); ?>
