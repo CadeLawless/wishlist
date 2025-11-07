2e78967b192d50f89bfc2da8a7cad5fd55de9741
Cade Lawless <clawless05@gmail.com>
2025-11-07 15:27:03 -0600

    Skip error popup when redirecting from homepage to login - Don't show 'Please log in to access this page' error when user tries to access homepage (/) - User knows they need to login to access homepage - Still show error popup for all other protected pages when redirecting to login

--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -15,8 +15,15 @@ class AuthMiddleware
     public function __invoke(Request $request): ?Response
     {
         if (!$this->authService->isLoggedIn()) {
-            return Response::redirect('/login')
-                ->withError('Please log in to access this page.');
+            $redirect = Response::redirect('/login');
+            
+            // Don't show error message if user is trying to access homepage
+            // User knows they need to login to access homepage
+            if ($request->path() !== '/') {
+                $redirect->withError('Please log in to access this page.');
+            }
+            
+            return $redirect;
         }
 
         return null; // Continue to next middleware/route
