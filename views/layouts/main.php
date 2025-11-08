9e927a469fa1da3173f035179a7cda408fa5e64b
Cade Lawless <clawless05@gmail.com>
2025-11-07 19:27:03 -0600

    Update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -6,7 +6,7 @@
     <link rel="icon" type="image/x-icon" href="/public/images/site-images/favicon.ico">
     <link rel="stylesheet" type="text/css" href="/public/css/styles.css" />
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    <title><?php echo $title ?? 'Wish List'; ?></title>
+    <title><?php echo $title ?? 'Any Wish List'; ?></title>
     <style>
         h1 {
             margin-top: 0;
