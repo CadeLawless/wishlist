f7d356b1393929e2dbb6137b021491d1bebf0328
Cade Lawless <clawless05@gmail.com>
2026-06-18 18:05:31 -0700

    fixed inline popup css

--- views/layouts/buyer.php
+++ views/layouts/buyer.php
@@ -16,7 +16,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="/public/images/site-images/favicon.ico">
-    <link rel="stylesheet" type="text/css" href="/public/css/styles.css?v=2.2" />
+    <link rel="stylesheet" type="text/css" href="/public/css/styles.css?v=2.2.1" />
     <link rel="stylesheet" type="text/css" href="/public/css/snow.css?v=2.2" />
     <script src="https://cdn.jsdelivr.net/npm/tsparticles-confetti@2.10.0/tsparticles.confetti.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
